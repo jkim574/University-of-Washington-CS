4f45c5cd9da3548a5fc6280e8108fb4c11463111
jkim574 <30674448+jkim574@users.noreply.github.com>
2020-04-15 21:01:37 +0900

    rearrange

--- assignment_2B.py
+++ assignment_2B.py
@@ -1,42 +1,19 @@
 #assignment#2
 
-SIZE = 3
+SIZE = 6
+
+
 
 def head_tail():
     for line in range(1, 2 * SIZE):
         print((2 * SIZE - line) * " " + line * "/" + "**" + line * "\\" + (2 * SIZE - line) * " ")
 
-def midLine():
+
+def mid_line():
     print("+" + 2 * SIZE * "=*" + "+")
 
-def firstHalf_top():
-    for line in range(1, SIZE + 1):
-        print("|" + (SIZE -line) * "." + \
-              line * "/\\" + \
-              (SIZE - line) * "." + \
-              (SIZE - line) * "." + \
-              line * "/\\" + \
-              (SIZE - line) * "." + "|")
 
-def firstHalf_bottom():
-    for line in range(SIZE, 0, -1):
-        print("|" + (SIZE - line) * "." + \
-              line * "\/" + \
-              (SIZE - line) * "." + \
-              (SIZE - line) * "." + \
-              line * "\/" + \
-              (SIZE - line) * "." + "|")
-
-def secondHalf_top():
-    for line in range(SIZE, 0, -1):
-        print("|" + (SIZE - line) * "." + \
-              line * "\/" + \
-              (SIZE - line) * "." + \
-              (SIZE - line) * "." + \
-              line * "\/" + \
-              (SIZE - line) * "." + "|")
-
-def secondHalf_bottom():
+def first_half_top():
     for line in range(1, SIZE + 1):
         print("|" + (SIZE -line) * "." + \
               line * "/\\" + \
@@ -46,12 +23,44 @@ def secondHalf_bottom():
               (SIZE - line) * "." + "|")
 
 
-head_tail()
-midLine()
-firstHalf_top()
-firstHalf_bottom()
-midLine()
-secondHalf_top()
-secondHalf_bottom()
-midLine()
-head_tail()
+def first_half_bottom():
+    for line in range(SIZE, 0, -1):
+        print("|" + (SIZE - line) * "." + \
+              line * "\/" + \
+              (SIZE - line) * "." + \
+              (SIZE - line) * "." + \
+              line * "\/" + \
+              (SIZE - line) * "." + "|")
+
+
+def second_half_top():
+    for line in range(SIZE, 0, -1):
+        print("|" + (SIZE - line) * "." + \
+              line * "\/" + \
+              (SIZE - line) * "." + \
+              (SIZE - line) * "." + \
+              line * "\/" + \
+              (SIZE - line) * "." + "|")
+
+
+def second_half_bottom():
+    for line in range(1, SIZE + 1):
+        print("|" + (SIZE -line) * "." + \
+              line * "/\\" + \
+              (SIZE - line) * "." + \
+              (SIZE - line) * "." + \
+              line * "/\\" + \
+              (SIZE - line) * "." + "|")
+
+
+
+if __name__ == "__main__":
+    head_tail()
+    mid_line()
+    first_half_top()
+    first_half_bottom()
+    mid_line()
+    second_half_top()
+    second_half_bottom()
+    mid_line()
+    head_tail()
